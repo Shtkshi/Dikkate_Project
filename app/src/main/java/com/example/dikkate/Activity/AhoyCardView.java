b0be9310e189ba828a5738065fdf5aaaa170698b
Shtkshi <shatakshi.tyagi22@gmail.com>
2021-02-10 20:46:19 +0530

    Cardview

---
+++ app/src/main/java/com/example/dikkate/Activity/AhoyCardView.java
@@ -0,0 +1,14 @@
+package com.example.dikkate.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AhoyCardView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ahoy_card_view);
+    }
+}
